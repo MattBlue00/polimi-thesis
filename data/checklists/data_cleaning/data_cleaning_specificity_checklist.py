1888128d0e4e16de24617b4bde37e76a57d05e57
MattBlue00 <matteo2.spreafico@mail.polimi.it>
2024-11-09 12:52:30 +0100

    new specificity prompt

--- data/checklists/data_cleaning/data_cleaning_specificity_checklist.py
+++ data/checklists/data_cleaning/data_cleaning_specificity_checklist.py
@@ -31,11 +31,29 @@ class DataCleaningSpecificityChecklist(BaseChecklist):
                 system_message=
                 """
 You are working on a dataset with 12 columns: brokered_by, status, price, bed, bath, acre_lot, street, city, state, zip_code, house_size, prev_sold_date. Assume that there is a dataframe containing the whole dataset.
-Keep in mind that there are only three types of missing values: '-', '' and NaN. Any other character or combination of characters must not be considered a missing value (for example, negative values are NOT to be considered missing values).
+Keep in mind that there are only three types of missing values: '-', '' and NaN. Any other character or combination of characters must not be considered a missing value (for example, negative values are NOT to be considered missing values). Remember that data type conversions with the parameter "errors='coerce'" handle ALL the mentioned kinds of missing values.
+You will be given a text that you will have to evaluate based on the fact that some proposed solutions are specific (score of 1) or general (score of 0).
 
-A set of solutions is said to be SPECIFIC if and only if, for the same kind of problem that is present for n columns, ALL n columns are given a different solution, tailored for the needs of that specific column. Otherwise, i.e. in the case at least two columns are being handled, for the same problem, with the same solution, the solutions for a given kind of problem are said to be GENERAL.
-Notice that if the solution for a problem is split into multiple parts, they ALL need to be specific for the set of solutions to be considered specific. This means that, for example, if there's a line of code that handles a certain type of missing value for all the columns and then all the columns' other missing values are handled specifically, the set of solutions must still be considered GENERAL.
-Keep also in mind that, typically, for loops that encompass different columns are clear signals of GENERAL solutions.               """,
+A problem is said to be handled in a SPECIFIC way if, for all the columns for which the text proposes a solution, the solution is given individually for each column.
+
+This means that if you have code that is similar to:
+for col in [col1, col2, col3]:
+    df[col] = do_something()
+Then, the solution 'do_something' is GENERAL, because it is applied to a set of more than one column in the exact same way.
+
+On the other hand, if you have code that is similar to:
+df[col1] = do_something()
+df[col2] = do_something()
+df[col3] = do_something()
+or similar to:
+df[col1] = do_something()
+df[col2] = do_something_else()
+df[col3] = do_something_different()
+Then, the set of solutions is SPECIFIC, because the text analyzes the problem for each column. It is not important if the proposed solutions, for each column, are the same or are different. The important thing to notice is whether they are separately mentioned (SPECIFIC) or not (GENERAL).
+
+Notice that, if there is AT LEAST one solution to a problem that is applied to all columns (e.g. table-wise replacement of '-'), then the problem is handled in a GENERAL way. IT DOES NOT MATTER if there are other alternative/complementary solutions that are specific for each column: as long as there's even one part of a problem that is handled in a general way, then the problem is said to be handled in a general way.
+Notice that if a problem is handled with for loop that involves more than one column, then the problem is handled in a GENERAL way.
+                """,
                 user_message=
                 """
 Consider if the piece of text I will give you provides a set of solutions that are specific for the various columns of the dataset or general for every column of the dataset for the following problems:
