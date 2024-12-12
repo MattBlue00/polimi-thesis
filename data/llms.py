8d21733b96d5b7fd28b69816bd5ae02fb6d5d4f8
MattBlue00 <matteo2.spreafico@mail.polimi.it>
2024-12-12 10:43:44 +0100

    small fix

--- data/llms.py
+++ data/llms.py
@@ -1,15 +1,15 @@
 from experiments.model.llm import GPT, Gemini, Mistral, Llama, Claude
 
 llms = [
-        #GPT(
-        #    model_name="gpt-4o-2024-08-06"
-        #),
-        #Gemini(
-        #    model_name="gemini-1.5-pro"
-        #),
-        #Claude(
-        #    model_name="claude-3-5-sonnet-latest"
-        #)
+        GPT(
+            model_name="gpt-4o-2024-08-06"
+        ),
+        Gemini(
+            model_name="gemini-1.5-pro"
+        ),
+        Claude(
+            model_name="claude-3-5-sonnet-latest"
+        ),
         #Mistral(
         #  model_name="mistral-large-2407"
         #),
