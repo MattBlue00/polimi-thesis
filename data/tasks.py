98e865528f99151bfa1d220397a4007b619ef4f0
MattBlue00 <matteo2.spreafico@mail.polimi.it>
2025-01-30 12:34:57 +0100

    fixed tasks to account for TableLlama

--- data/tasks.py
+++ data/tasks.py
@@ -23,7 +23,10 @@ tasks = [
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do data cleaning on it? Let's think step by step.",
                 system_message="You are a data quality expert. For this reason, you help users by providing detailed answers that stick to the task and dataset you are given. Be the most accurate and complete you can.",
             )
-    ]),
+        ],
+        tab_instruction="This is a Data Cleaning task. The goal of this task is to provide a clean version of the given dataset.",
+        tab_question="Can you do Data Cleaning on the given dataset?"
+    ),
     Task(
         name="data_profiling",
         prompts=[
@@ -45,7 +48,9 @@ tasks = [
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do data profiling on it? Let's think step by step.",
                 system_message="You are a data quality expert. For this reason, you help users by providing detailed answers that stick to the task and dataset you are given. Be the most accurate and complete you can.",
             )
-        ]
+        ],
+        tab_instruction="This is a Data Profiling task. The goal of this task is to provide insights and statistics about the given dataset.",
+        tab_question="Can you do Data Profiling on the given dataset?"
     ),
     Task(
         name="data_imputation",
@@ -68,7 +73,9 @@ tasks = [
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do data imputation on it? Let's think step by step.",
                 system_message="You are a data quality expert. For this reason, you help users by providing detailed answers that stick to the task and dataset you are given. Be the most accurate and complete you can.",
             )
-        ]
+        ],
+        tab_instruction="This is a Data Imputation task. The goal of this task is to provide an imputed version of the given dataset.",
+        tab_question="Can you do Data Imputation on the given dataset?"
     ),
     Task(
         name="data_deduplication",
@@ -91,7 +98,9 @@ tasks = [
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do data deduplication on it? Let's think step by step.",
                 system_message="You are a data quality expert. For this reason, you help users by providing detailed answers that stick to the task and dataset you are given. Be the most accurate and complete you can.",
             )
-        ]
+        ],
+        tab_instruction="This is a Data Deduplication task. The goal of this task is to provide a deduplicated version of the given dataset.",
+        tab_question="Can you do Data Deduplication on the given dataset?"
     ),
     Task(
         name="outlier_detection",
@@ -114,7 +123,9 @@ tasks = [
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do outlier detection on it? Let's think step by step.",
                 system_message="You are a data quality expert. For this reason, you help users by providing detailed answers that stick to the task and dataset you are given. Be the most accurate and complete you can.",
             )
-        ]
+        ],
+        tab_instruction="This is an Outlier Detection task. The goal of this task is to provide the outliers of the given dataset.",
+        tab_question="Can you do Outlier Detection on the given dataset?"
     ),
     Task(
         name="data_wrangling",
@@ -123,7 +134,9 @@ tasks = [
                 prompt_id=1,
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do data wrangling on it?"
             ),
-        ]
+        ],
+        tab_instruction="This is a Data Wrangling task. The goal of this task is to provide a correctly structured version of the given dataset.",
+        tab_question="Can you do Data Wrangling on the given dataset?"
     ),
     Task(
         name="data_standardization",
@@ -132,7 +145,9 @@ tasks = [
                 prompt_id=1,
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do data standardization on it?"
             ),
-        ]
+        ],
+        tab_instruction="This is a Data Standardization task. The goal of this task is to provide a standardized version of the given dataset.",
+        tab_question="Can you do Data Standardization on the given dataset?"
     ),
     Task(
         name="dependency_discovery",
@@ -141,7 +156,9 @@ tasks = [
                 prompt_id=1,
                 user_message="Consider this dataset:\n{{csv_text}}\nCan you do dependency discovery on it?"
             ),
-        ]
+        ],
+        tab_instruction="This is a Dependency Discovery task. The goal of this task is to provide the functional dependencies among the columns of the given dataset.",
+        tab_question="Can you do Dependency Discovery on the given dataset?"
     ),
 ]
 
@@ -152,4 +169,4 @@ def get_prompt(task_name, prompt_id):
                 if str(prompt.id) == str(prompt_id):
                     return prompt
 
-    raise Exception("Prompt " + str(prompt_id) + " not found for task " + str(task_name))
+    raise ValueError("Prompt " + str(prompt_id) + " not found for task " + str(task_name))
--- experiments/model/task.py
+++ experiments/model/task.py
@@ -1,5 +1,7 @@
 class Task:
 
-    def __init__(self, name, prompts):
+    def __init__(self, name, prompts, tab_instruction, tab_question):
         self.name = name
         self.prompts = prompts
+        self.tab_instruction = tab_instruction
+        self.tab_question = tab_question
